187fd6e07a79dd4a785e7e8fd03e69d90f33ce97
ErikaP <108128810+ErikaPrice996@users.noreply.github.com>
2024-09-11 15:17:13 -0400

    Add files via upload

--- Register.php
+++ Register.php
@@ -9,17 +9,34 @@
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError($conn->connect_error);
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$stmt = $conn->prepare("SELECT count(*) FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $login);
 		$stmt->execute();
+		$stmt->bind_result($count);
+		$stmt->fetch();
+		
+		if ($count != 0) 
+		{
+     		$stmt->close();
+			$conn->close();
+			returnWithError("Username already taken");
+		} 
+		else 
+		{    
+			$stmt->close();
 
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+			$stmt->execute();
+
+			$stmt->close();
+			$conn->close();
+			returnWithSuccess("User successfully registered, please Log in");
+		}
 	}
 	
 	function getRequestInfo()
@@ -27,15 +44,21 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
+	function returnWithError($err)
 	{
 		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithSuccess($msg)
+	{
+		$retValue = '{"success":"' . $msg . '"}';
+		sendResultInfoAsJson($retValue);
 	}
 ?>
