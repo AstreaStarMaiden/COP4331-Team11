05fc6c188a80e23936abf6471351a324ce39ee19
ErikaP <108128810+ErikaPrice996@users.noreply.github.com>
2024-09-15 10:22:19 -0400

    Add files via upload
    Most up to date files as of 9/15
    
    currently search working, with Edit and Delete buttons added to search results but not implemented.
    Current Site layout in close to final setting.
    APIs unchanged but will be updated with string verification code.

---
+++ LAMPAPI/Edit.php
@@ -0,0 +1,47 @@
+<?php
+
+	$inData = getRequestInfo();
+
+    $newName = $inData["newName"];
+    $newPhone = $inData["newPhone"];
+    $newEmail = $inData["newEmail"];
+	  $userId = $inData["UserId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE Name =? AND UserID =? AND Phone =? AND Email =?");
+		$stmt->bind_param("ssss", $inData["Name"], $inData["UserId"], $inData["Phone"], $inData["Email"]);
+		$stmt->execute();
+		$stmt->close();
+
+    $stmt = $conn->prepare("INSERT into Contacts (UserId,Name,Phone,Email) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $userId, $newName, $newPhone, $newEmail);
+		$stmt->execute();
+		$stmt->close();
+		
+		$conn->close();
+		returnWithError("");
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"Name":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
---
+++ LAMPAPI/Register.php
@@ -0,0 +1,64 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError($conn->connect_error);
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT count(*) FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$stmt->bind_result($count);
+		$stmt->fetch();
+		
+		if ($count != 0) 
+		{
+     	$stmt->close();
+			$conn->close();
+			returnWithError("Username already taken");
+		} 
+		else 
+		{    
+			$stmt->close();
+
+			$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+			$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+			$stmt->execute();
+
+			$stmt->close();
+			$conn->close();
+			returnWithSuccess("User successfully registered, please log in");
+		}
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+
+	function returnWithSuccess($msg)
+	{
+		$retValue = '{"success":"' . $msg . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+?>
